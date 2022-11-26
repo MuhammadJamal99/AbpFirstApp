d69b35454945f007af2ecaff970535c05a48912e
MuhammadJamal99 <muhammadjammi18@gmail.com>
2022-11-26 12:10:18 +0200

    Add Book Model Update, and Seed  Database

---
+++ src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
@@ -0,0 +1,38 @@
+﻿using Acme.BookStore.Books;
+using System.Threading.Tasks;
+using System;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acme.BookStore;
+public class BookStoreDataSeederContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Book, Guid> BookRepository;
+
+    public BookStoreDataSeederContributor(IRepository<Book, Guid> bookRepository)
+    {
+        BookRepository = bookRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await BookRepository.GetCountAsync() <= 10)
+        {
+            List<Book> BooksList = new();
+            for (int i = 1; i < 1_000; i++)
+            {
+                BooksList.Add(new Book()
+                {
+                    Name = $"Book {i}",
+                    Type = BookType.Biography,
+                    PublishDate = DateTime.Now,
+                    Price = i + 100
+                });
+            }
+            await BookRepository.InsertManyAsync(BooksList.AsEnumerable());
+        }
+    }
+}
